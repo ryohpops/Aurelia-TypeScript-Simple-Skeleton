aab6efce356fda26767e696e057f4f333dc7f796
ryohpops <ryohpops@users.noreply.github.com>
2015-07-23 13:16:24 +0900

    Adapt to Visual Studio 2015.

--- Aurelia-TypeScript-Simple-Skeleton/app.ts
+++ Aurelia-TypeScript-Simple-Skeleton/app.ts
@@ -1,5 +1,7 @@
-﻿export class App {
-    configureRouter(config: any, router: any) {
+﻿import {Router, RouterConfiguration} from "aurelia-router";
+
+export class App {
+    configureRouter(config: RouterConfiguration, router: Router) {
         config.title = "Aurelia";
         config.map([
             { route: ["", "welcome"], name: "welcome", moduleId: "app/welcome", nav: true, title: "Welcome" }
